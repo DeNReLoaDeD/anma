3e7078508a073ad88073d3b0f8faaf3bffd2b687
tonimontero <toni.montero@agoraimages.com>
2018-11-18 13:01:08 +0100

    initial commit

---
+++ src/main/java/es/anma/articles/AnmaApplication.java
@@ -0,0 +1,12 @@
+package es.anma.articles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnmaApplication.class, args);
+	}
+}
